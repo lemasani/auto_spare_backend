664e250527523f3494c7408a3fb055a871efbd92
lemasani <brianlemasan@gmail.com>
2024-08-05 21:15:45 +0300

    feat: Update body parser middleware in index.js
    This commit updates the body parser middleware in the `index.js` file to include support for parsing URL-encoded data. The `express.urlencoded()` function is added with the `extended` option set to `true` to enable parsing of nested objects.

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const app = express()
 
 const port = process.env.PORT || 8000
 
-app.use(express.json())
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 // Routes
